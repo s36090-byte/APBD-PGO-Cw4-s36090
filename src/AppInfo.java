7f6a8864b05e150948a3d2a80bc7f19db22f0acc
s36090-byte <s36090@pjwstk.edu.pl>
2026-03-22 09:54:20 +0100

    dodanie komentarzy w AppInfo

--- src/AppInfo.java
+++ src/AppInfo.java
@@ -1,12 +1,13 @@
 public class AppInfo {
-    private String name;
-    private String version;
+    private String name; //nazwa programu
+    private String version; //wersja
 
     public AppInfo(String name, String version) {
         this.name = name;
         this.version = version;
     }
 
+    //wyswietla info o prgmie
     public void show() {
         System.out.println("Program: " + name);
         System.out.println("Wersja: " + version);
