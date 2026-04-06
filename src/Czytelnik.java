1171b290cb1fe73dcc1f5577bc39adb1f8ab9df8
s36090-byte <s36090@pjwstk.edu.pl>
2026-04-06 21:09:19 +0200

    Dodanie klasy Czytelnik i metod pomocniczych

---
+++ src/Czytelnik.java
@@ -0,0 +1,39 @@
+public class Czytelnik {
+    private String imie;
+    private String nazwisko;
+    private String numerKarty;
+    private int liczbaWypozyczen;
+
+    // konstruktor
+    public Czytelnik(String imie, String nazwisko, String numerKarty, int liczbaWypozyczen) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.numerKarty = numerKarty;
+        this.liczbaWypozyczen = liczbaWypozyczen;
+    }
+
+    // wypisanie danych
+    public void wypiszDane() {
+        System.out.println("Imię: " + imie);
+        System.out.println("Nazwisko: " + nazwisko);
+        System.out.println("Numer karty: " + numerKarty);
+        System.out.println("Liczba wypożyczeń: " + liczbaWypozyczen);
+        System.out.println("----------------------");
+    }
+
+    // zwiększanie liczby wypożyczeń
+    public void zwiekszLiczbeWypozyczen() {
+        liczbaWypozyczen++;
+        System.out.println(imie + " wypożyczył książkę.");
+    }
+
+    // zmniejszanie liczby wypożyczeń
+    public void zmniejszLiczbeWypozyczen() {
+        if (liczbaWypozyczen > 0) {
+            liczbaWypozyczen--;
+            System.out.println(imie + " zwrócił książkę.");
+        } else {
+            System.out.println("Brak wypożyczeń do zmniejszenia.");
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,22 @@ public class Main {
     Ksiazka k3 = new Ksiazka("Lalka", "Prus", 400, false);
 
     k1.wypiszInfo();
+    // czytelnicy
+    Czytelnik c1 = new Czytelnik("Jan", "Kowalski", "123", 0);
+    Czytelnik c2 = new Czytelnik("Anna", "Nowak", "456", 2);
+
+    // test metod
+    c1.wypiszDane();
+    c2.wypiszDane();
+
+    c1.zwiekszLiczbeWypozyczen();
+    c1.zwiekszLiczbeWypozyczen();
+
+    c2.zmniejszLiczbeWypozyczen();
+
+    // sprawdzenie po zmianach
+    c1.wypiszDane();
+    c2.wypiszDane();
     k2.wypiszInfo();
     k3.wypiszInfo();
 
